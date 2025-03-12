c87ccf24170d889252a33b72611f9b10621da969
oliverconstance <oliver.j.constance@gmail.com>
2025-03-12 08:02:07 +0000

    first commit of structure

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+// You can add JavaScript code here for any interactivity you need.
+// For now, it's empty as the basic structure doesn't require complex JS.
+// However, you might add things like:
+// - Smooth scrolling to sections
+// - Form validation
+// - Animations
+
+// Example (smooth scroll) - Uncomment if you want this feature:
+// document.querySelectorAll('nav a').forEach(anchor => {
+//     anchor.addEventListener('click', function (e) {
+//         e.preventDefault();
+
+//         document.querySelector(this.getAttribute('href')).scrollIntoView({
+//             behavior: 'smooth'
+//         });
+//     });
+// });
